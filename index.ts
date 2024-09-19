935e9d22cf2242e1353328e5223c6ab67e18f3a2
alecdwm <alec@owls.io>
2024-09-19 06:54:00 +0000

    fix: use withPolkadotSdkCompat

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import { avail } from '@polkadot-api/descriptors'
 import { createClient } from 'polkadot-api'
+import { withPolkadotSdkCompat } from 'polkadot-api/polkadot-sdk-compat'
 import { getWsProvider } from 'polkadot-api/ws-provider/web'
 
 async function main(rpcUrl: string) {
-  const provider = getWsProvider(rpcUrl)
-  const client = createClient(provider)
+  const client = createClient(withPolkadotSdkCompat(getWsProvider(rpcUrl)))
   const api = client.getTypedApi(avail)
 
   try {
